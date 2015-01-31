8ce807b393a9404d096c8f093a7d0aa928e94631
鈴木貴明 <teeaki@suzukitakaaki-no-MacBook-Air.local>
2015-01-31 22:39:23 +0900

    scoped array compare origin value or converted value

--- lib/active_hash/scoped_array.rb
+++ lib/active_hash/scoped_array.rb
@@ -15,25 +15,26 @@ module ActiveHash
 
     def where(options = nil)
       return self if options.nil?
+      converted_options = {}
       options.each {|name, value|
         foptions = @klass.field_options[name]
         next unless foptions && (type = foptions[:type]) && value != nil
         convert = ActiveHash.converters[type]
         if value.is_a?(Array)
-          options[name] = value.map{|val|convert.try(val)}
+          converted_options[name] = value.map{|val|convert.try(val)}
         elsif !value.is_a?(Range)
-          options[name] = convert.try(value)
+          converted_options[name] = convert.try(value)
         end
       }
       self.class.new(select do |record|
         options.all? do |col, match|
           attr = record[col]
           if match.is_a?(Array)
-            match.any? {|val| attr == val }
+            match.any? {|val| attr == val } || (converted_options[col] && converted_options[col].any? {|val| attr == val })
           elsif match.is_a?(Range)
             match.first <= attr && match.last >= attr
           else
-            attr == match
+            attr == match || (converted_options[col] && attr == converted_options[col])
           end
         end
       end, @klass)
